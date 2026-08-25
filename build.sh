6b2418612ddb8dc3c8b72b668b8eeca1375a9511
vistone <3184972+vistone@users.noreply.github.com>
2026-08-25 16:56:22 +0800

    build: keep all build outputs and runtime deps under bin/

--- build.sh
+++ build.sh
@@ -12,9 +12,10 @@ else
   version="unstable"
 fi
 # https://github.com/webpack/webpack/issues/14532#issuecomment-947012063
-cd "$CurrentDir"/gui && yarn --ignore-engines && OUTPUT_DIR="$CurrentDir"/service/server/router/web yarn --ignore-engines build
-
 mkdir -p "$CurrentDir"/bin
+# GUI 构建产物统一进 bin/web，运行时用 --webdir 指向（源码区 service/server/router/web 仅保留 embed 占位）
+cd "$CurrentDir"/gui && yarn --ignore-engines && OUTPUT_DIR="$CurrentDir"/bin/web yarn --ignore-engines build
+
 # Build v2raya-core (merged xray-core + custom protocols)
 cd "$CurrentDir"/core && CGO_ENABLED=0 go build -trimpath -ldflags "-X main.Version=$version -s -w" -o "$CurrentDir"/bin/v2raya_core ./main
 
