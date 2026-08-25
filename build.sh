176fbe10970f33c903768cdf26c73ce4b2940ce5
vistone <3184972+vistone@users.noreply.github.com>
2026-08-25 16:53:12 +0800

    build: output binaries to bin/ directory

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ else
 fi
 # https://github.com/webpack/webpack/issues/14532#issuecomment-947012063
 cd "$CurrentDir"/gui && yarn --ignore-engines && OUTPUT_DIR="$CurrentDir"/service/server/router/web yarn --ignore-engines build
-# Build v2raya-core (merged xray-core + custom protocols)
-cd "$CurrentDir"/core && CGO_ENABLED=0 go build -trimpath -ldflags "-X main.Version=$version -s -w" -o "$CurrentDir"/v2raya_core ./main
 
-cd "$CurrentDir"/service && CGO_ENABLED=0 go build -trimpath -tags "with_gvisor" -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$CurrentDir"/v2raya
+mkdir -p "$CurrentDir"/bin
+# Build v2raya-core (merged xray-core + custom protocols)
+cd "$CurrentDir"/core && CGO_ENABLED=0 go build -trimpath -ldflags "-X main.Version=$version -s -w" -o "$CurrentDir"/bin/v2raya_core ./main
+
+cd "$CurrentDir"/service && CGO_ENABLED=0 go build -trimpath -tags "with_gvisor" -ldflags "-X github.com/v2rayA/v2rayA/conf.Version=$version -s -w" -o "$CurrentDir"/bin/v2raya
